bc4816615e553571704b1ecf49f018b280cc37e5
leox00 <146089334+leox00@users.noreply.github.com>
2024-04-19 18:21:27 +0200

    Trace IP tool logic
    Added logic to the trace ip tool

--- ToolKit.bat
+++ ToolKit.bat
@@ -22,13 +22,15 @@ echo.
 echo     0) Help
 echo     1) Pc info
 echo     2) SMB Bruteforce
+echo     3) Trace IP
 echo.
 set /p menuInput=">> "
 set /a save=0
 if /I "%menuInput:~1,2%" EQU "s" (set /a save=1) else (set /a save=0)
 if /I "%menuInput:~0,1%" EQU "0" goto help
-if /I "%menuInput:~0,1%" EQU "1" goto pcinfo
+if /I "%menuInput:~0,1%" EQU "1" goto pcInfo
 if /I "%menuInput:~0,1%" EQU "2" goto smb
+if /I "%menuInput:~0,1%" EQU "3" goto traceIp
 echo Invalid Tool. Try again!
 pause
 goto menu
@@ -44,15 +46,17 @@ echo         HELP
 echo       ---------
 echo     1) Pc info
 echo     2) SMB Bruteforce
+echo     3) Trace IP
 echo.
 set /p helpInput=">> "
-if /I "%helpInput%" EQU "1" goto helpPcinfo
+if /I "%helpInput%" EQU "1" goto helpPcInfo
 if /I "%helpInput%" EQU "2" goto helpSmb
+if /I "%helpInput%" EQU "3" goto helpTraceIp
 echo Invalid help. Try again!
 pause
 goto help
 
-:helpPcinfo
+:helpPcInfo
 cls
 title Help Pc Info
 type "help.txt"
@@ -70,8 +74,17 @@ echo The SMB Bruteforcer can...
 pause
 goto menu
 
+:helpTraceIp
+cls
+title Help Trace IP
+type "help.txt"
+echo.
+echo The Trace IP tool can...
+pause
+goto menu
+
 REM Pc Info
-:pcinfo
+:pcInfo
 cls
 title Pc Info
 type "pcinfo.txt"
@@ -97,9 +110,9 @@ echo       ---------
 echo     1) With user and password
 echo     2) Only ip (usefull to connect to localhost etc.)
 echo.
-set /p smbinput=">> "
-if /I "%smbinput%" EQU "1" goto smbUsrPsswrd
-if /I "%smbinput%" EQU "2" goto smbOnlyIp
+set /p smbInput=">> "
+if /I "%smbInput%" EQU "1" goto smbUsrPsswrd
+if /I "%smbInput%" EQU "2" goto smbOnlyIp
 echo Invalid SMB Tool. Try again!
 pause
 goto smb
@@ -156,5 +169,24 @@ net use \\%ip% /user:%user% %pass% >nul 2>&1
 echo [ATTEMPT %count%] [%pass%]
 set /a count=%count%+1
 if %errorlevel% EQU 0 goto success
+
+REM Trace IP
+:traceIp
+cls
+title Trace IP
+type "traceip.txt"
+echo.
+set /p ip="Enter the target IP>> "
+curl -s https://api.findip.net/%ip%/?token=01e50d025741485196e37ee3567be22c -o ipinfo.json
+echo.
+echo      %ip%
+echo       ---------
+echo      Info 1
+echo      Info 2
+echo      Info 3
+echo.
+pause
+"" > ipinfo.json
+goto menu
 
 REM Others
