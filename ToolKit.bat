cc7b01c1e6e8a98156239a883a6571030026e0dd
leox00 <146089334+leox00@users.noreply.github.com>
2024-04-20 23:11:13 +0200

    Trace ip finished
    Trace ip finished and started its save logic but bugs

--- ToolKit.bat
+++ ToolKit.bat
@@ -12,7 +12,7 @@ REM Menu
 cls
 title TOOLKIT
 echo.
-type "menubanner.txt"
+type ".\banners\menubanner.txt"
 echo.
 echo.
 echo         TOOLS
@@ -26,7 +26,7 @@ echo     3) Trace IP
 echo.
 set /p menuInput=">> "
 set /a save=0
-if /I "%menuInput:~1,2%" EQU "s" (set /a save=1) else (set /a save=0)
+if /I "%menuInput:~1,2%" EQU "s" (set /a save=1)
 if /I "%menuInput:~0,1%" EQU "0" goto help
 if /I "%menuInput:~0,1%" EQU "1" goto pcInfo
 if /I "%menuInput:~0,1%" EQU "2" goto smb
@@ -39,16 +39,18 @@ REM Help
 :help
 cls
 title Help
-type "help.txt"
+type ".\banners\help.txt"
 echo.
 echo.
 echo         HELP
 echo       ---------
+echo     0) Return to menu
 echo     1) Pc info
 echo     2) SMB Bruteforce
 echo     3) Trace IP
 echo.
 set /p helpInput=">> "
+if /I "%helpInput%" EQU "0" goto menu
 if /I "%helpInput%" EQU "1" goto helpPcInfo
 if /I "%helpInput%" EQU "2" goto helpSmb
 if /I "%helpInput%" EQU "3" goto helpTraceIp
@@ -59,7 +61,7 @@ goto help
 :helpPcInfo
 cls
 title Help Pc Info
-type "help.txt"
+type ".\banners\help.txt"
 echo.
 echo The Pc Info tool can...
 pause
@@ -68,7 +70,7 @@ goto menu
 :helpSmb
 cls
 title Help SMB
-type "help.txt"
+type ".\banners\help.txt"
 echo.
 echo The SMB Bruteforcer can...
 pause
@@ -77,7 +79,7 @@ goto menu
 :helpTraceIp
 cls
 title Help Trace IP
-type "help.txt"
+type ".\banners\help.txt"
 echo.
 echo The Trace IP tool can...
 pause
@@ -87,10 +89,10 @@ REM Pc Info
 :pcInfo
 cls
 title Pc Info
-type "pcinfo.txt"
+type ".\banners\pcinfo.txt"
 echo.
-if %save% EQU 0 (systeminfo) else (
-    systeminfo
+systeminfo
+if %save% EQU 1 (
     echo PCINFO OUTPUT > ../save/pcInfo.txt
     echo. >> ../save/pcInfo.txt
     systeminfo >> ../save/pcInfo.txt
@@ -102,15 +104,17 @@ REM SMB Bruteforce
 :smb
 cls
 title SMB Bruteforce
-type "smb.txt"
+type ".\banners\smb.txt"
 echo.
 echo.
 echo       SMB TOOLS
 echo       ---------
+echo     0) Return to menu
 echo     1) With user and password
 echo     2) Only ip (usefull to connect to localhost etc.)
 echo.
 set /p smbInput=">> "
+if /I "%smbInput%" EQU "0" goto menu
 if /I "%smbInput%" EQU "1" goto smbUsrPsswrd
 if /I "%smbInput%" EQU "2" goto smbOnlyIp
 echo Invalid SMB Tool. Try again!
@@ -120,7 +124,7 @@ goto smb
 :smbUsrPsswrd
 cls
 title SMB Bruteforce User, Password
-type "smb.txt"
+type ".\banners\smb.txt"
 echo.
 set /p ip="Enter the target IP>> "
 set /p user="Enter the target user>> "
@@ -139,7 +143,7 @@ goto menu
 :smbOnlyIp
 cls
 title SMB Bruteforce Only Ip
-type "smb.txt"
+type ".\banners\smb.txt"
 echo.
 set /p ip="Enter the target IP>> "
 net use \\%ip% >nul 2>&1
@@ -174,17 +178,39 @@ REM Trace IP
 :traceIp
 cls
 title Trace IP
-type "traceip.txt"
+type ".\banners\traceip.txt"
 echo.
 set /p ip="Enter the target IP>> "
 curl -s https://api.findip.net/%ip%/?token=01e50d025741485196e37ee3567be22c -o ipinfo.json
+for /F %%i in ('.\jq.exe .continent.names.en ipinfo.json') do set continent=%%i
+for /F %%i in ('.\jq.exe .country.names.en ipinfo.json') do set country=%%i
+for /F %%i in ('.\jq.exe .city.names.en ipinfo.json') do set city=%%i
+for /F %%i in ('.\jq.exe .location.latitude ipinfo.json') do set latitude=%%i
+for /F %%i in ('.\jq.exe .location.longitude ipinfo.json') do set longitude=%%i
+for /F %%i in ('.\jq.exe .location.time_zone ipinfo.json') do set time_zone=%%i
 echo.
 echo      %ip%
 echo       ---------
-echo      Info 1
-echo      Info 2
-echo      Info 3
+echo      Continent: %continent%
+echo      Country: %country%
+echo      City: %city%"
+echo      Coordinates(lat, long): %latitude% %longitude%
+echo      Time zone: %time_zone%
 echo.
+if %save% EQU 1 (
+    echo TRACEIP OUTPUT > ../save/traceIP.txt
+    echo. >> ../save/traceIP.txt
+    echo %ip% >> ../save/traceIP.txt
+    echo  --------- >> ../save/traceIP.txt
+    echo Continent: %continent% >> ../save/traceIP.txt
+    echo Country: %country% >> ../save/traceIP.txt
+    echo City: %city% >> ../save/traceIP.txt
+    echo Coordinates(lat, long): %latitude% %longitude% >> ../save/traceIP.txt
+    echo Time zone: %time_zone% >> ../save/traceIP.txt
+    pause
+    "" > ipinfo.json
+    goto menu
+)
 pause
 "" > ipinfo.json
 goto menu
